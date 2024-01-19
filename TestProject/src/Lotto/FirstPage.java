56fec71084be0177701c605ad8c2d3640ec72d9b
dongso03 <dongso03@naver.com>
2024-01-19 12:09:08 +0900

    배경넣음

--- TestProject/src/Lotto/FirstPage.java
+++ TestProject/src/Lotto/FirstPage.java
@@ -31,7 +31,7 @@ public class FirstPage extends JFrame {
 		winningGUI = new WinningGUI(this);
 		purchaseGUI = new PurchaseGUI(this, chargGUI);
 		ImageIcon icon = new ImageIcon("Image/캡처.PNG");
-			
+
 		extracted();
 		btnCharge.addActionListener(new ActionListener() {
 			@Override
@@ -57,8 +57,7 @@ public class FirstPage extends JFrame {
 				winningGUI.setVisible(true);
 			}
 		});
-		
-		
+
 		showGUI();
 	}
 
@@ -71,52 +70,52 @@ public class FirstPage extends JFrame {
 	private void extracted() {
 		SpringLayout springLayout = new SpringLayout();
 		getContentPane().setLayout(springLayout);
-		
+
 		panel = new JPanel();
 		springLayout.putConstraint(SpringLayout.NORTH, panel, 0, SpringLayout.NORTH, getContentPane());
 		springLayout.putConstraint(SpringLayout.WEST, panel, 0, SpringLayout.WEST, getContentPane());
 		springLayout.putConstraint(SpringLayout.SOUTH, panel, 0, SpringLayout.SOUTH, getContentPane());
 		springLayout.putConstraint(SpringLayout.EAST, panel, 783, SpringLayout.WEST, getContentPane());
 		getContentPane().add(panel);
-				panel.setLayout(null);
-		
-				btnPuchase = new JButton("구매하기");
-				btnPuchase.setBounds(56, 179, 98, 131);
-				panel.add(btnPuchase);
-				springLayout.putConstraint(SpringLayout.SOUTH, btnPuchase, -10, SpringLayout.SOUTH, getContentPane());
-				btnPuchase.addActionListener(new ActionListener() {
-					public void actionPerformed(ActionEvent arg0) {
-					}
-				});
-				springLayout.putConstraint(SpringLayout.NORTH, btnPuchase, 17, SpringLayout.SOUTH, panel);
-				springLayout.putConstraint(SpringLayout.WEST, btnPuchase, 84, SpringLayout.EAST, btnCharge);
-		
+		panel.setLayout(null);
+
+		btnPuchase = new JButton("구매하기");
+		btnPuchase.setBounds(56, 179, 98, 131);
+		panel.add(btnPuchase);
+		springLayout.putConstraint(SpringLayout.SOUTH, btnPuchase, -10, SpringLayout.SOUTH, getContentPane());
+		btnPuchase.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+			}
+		});
+		springLayout.putConstraint(SpringLayout.NORTH, btnPuchase, 17, SpringLayout.SOUTH, panel);
+		springLayout.putConstraint(SpringLayout.WEST, btnPuchase, 84, SpringLayout.EAST, btnCharge);
+
 		btnCharge = new JButton("충전하기");
 		btnCharge.setBounds(251, 179, 98, 131);
 		panel.add(btnCharge);
-		
-				springLayout.putConstraint(SpringLayout.NORTH, btnCharge, -151, SpringLayout.SOUTH, getContentPane());
-				springLayout.putConstraint(SpringLayout.WEST, btnCharge, 57, SpringLayout.WEST, getContentPane());
-				springLayout.putConstraint(SpringLayout.SOUTH, btnCharge, 0, SpringLayout.SOUTH, getContentPane());
-				springLayout.putConstraint(SpringLayout.EAST, btnCharge, 207, SpringLayout.WEST, getContentPane());
-				
-						btnWinning = new JButton("당첨확인");
-						btnWinning.setBounds(422, 179, 111, 131);
-						panel.add(btnWinning);
-						springLayout.putConstraint(SpringLayout.WEST, btnWinning, 529, SpringLayout.WEST, getContentPane());
-						springLayout.putConstraint(SpringLayout.NORTH, btnWinning, 233, SpringLayout.NORTH, getContentPane());
-						springLayout.putConstraint(SpringLayout.SOUTH, btnWinning, -21, SpringLayout.SOUTH, getContentPane());
-						springLayout.putConstraint(SpringLayout.EAST, btnWinning, -105, SpringLayout.EAST, getContentPane());
-						springLayout.putConstraint(SpringLayout.EAST, btnPuchase, -93, SpringLayout.WEST, btnWinning);
-						
-						lblNewLabel = new JLabel("");
-						lblNewLabel.setIcon(new ImageIcon(FirstPage.class.getResource("/Image/캡처.PNG")));
-						lblNewLabel.setBounds(0, 0, 783, 405);
-						panel.add(lblNewLabel);
-				btnCharge.addActionListener(new ActionListener() {
-					public void actionPerformed(ActionEvent e) {
-					}
-				});
+
+		springLayout.putConstraint(SpringLayout.NORTH, btnCharge, -151, SpringLayout.SOUTH, getContentPane());
+		springLayout.putConstraint(SpringLayout.WEST, btnCharge, 57, SpringLayout.WEST, getContentPane());
+		springLayout.putConstraint(SpringLayout.SOUTH, btnCharge, 0, SpringLayout.SOUTH, getContentPane());
+		springLayout.putConstraint(SpringLayout.EAST, btnCharge, 207, SpringLayout.WEST, getContentPane());
+
+		btnWinning = new JButton("당첨확인");
+		btnWinning.setBounds(422, 179, 111, 131);
+		panel.add(btnWinning);
+		springLayout.putConstraint(SpringLayout.WEST, btnWinning, 529, SpringLayout.WEST, getContentPane());
+		springLayout.putConstraint(SpringLayout.NORTH, btnWinning, 233, SpringLayout.NORTH, getContentPane());
+		springLayout.putConstraint(SpringLayout.SOUTH, btnWinning, -21, SpringLayout.SOUTH, getContentPane());
+		springLayout.putConstraint(SpringLayout.EAST, btnWinning, -105, SpringLayout.EAST, getContentPane());
+		springLayout.putConstraint(SpringLayout.EAST, btnPuchase, -93, SpringLayout.WEST, btnWinning);
+
+		lblNewLabel = new JLabel("");
+		lblNewLabel.setIcon(new ImageIcon(FirstPage.class.getResource("/Image/캡처.PNG")));
+		lblNewLabel.setBounds(0, 0, 783, 405);
+		panel.add(lblNewLabel);
+		btnCharge.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
 	}
 
 	public static void main(String[] args) {
